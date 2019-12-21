344fd7d25cc6c19cc8054ba5b34645883cbcde9f
styvens59 <56022040+styvens59@users.noreply.github.com>
2019-12-21 15:46:24 +0100

    MàJ: ajout de 22 question

--- base.js
+++ base.js
@@ -11,8 +11,8 @@ const questObj = [
     },
     {
         "question": "En 1794, quel baron, mort à la bataille de la Moskova, est à l’origine de l’arrestation de Robespierre ?",
-        "bonne_reponse": 'MERDA (CHARLES-ANDRE)',
-        "reponse": ['MERDA (CHARLES-ANDRE)', 'NAPOLÉON 1ER', 'MIKHAÏL KOUTOUZOV', "IL C'EST RENDU LUI MÊME"],
+        "bonne_reponse": 'CHARLES-ANDRE MERDA',
+        "reponse": ['CHARLES-ANDRE MERDA', 'NAPOLÉON 1ER', 'MIKHAÏL KOUTOUZOV', "IL C'EST RENDU LUI MÊME"],
     },
     {
         "question": "Quel genre d’animal est la tégénaire qui tisse des toiles dans les angles des greniers et des caves ?",
@@ -39,8 +39,117 @@ const questObj = [
         "bonne_reponse": "GEORGE WEAH",
         "reponse": ["THÉOPHILE ABEGA", "SALIF KEÏTA", "ROGER MILLA", "GEORGE WEAH"],
     },
+    {
+        "question": "Quel grammairien français, évoqué dans les Femmes Savantes a été l’un des premiers membres de l’Académie Française ?",
+        "bonne_reponse": "CLAUDE FAVRE",
+        "reponse": ["CLAUDE FAVRE", "ÉMILE EGGER", "FRANÇOIS NOËL", "LOUIS DOMAIRON"],
+    },
+    {
+        "question": "En médecine, quelle infection virale courante est une inflammation des amygdales ?",
+        "bonne_reponse": "UNE ANGINE",
+        "reponse": ["UNE ANGINE", "UNE PHARYNGITE", "LA GONORRHÉE", "LA GRIPPE"],
+    },
+    {
+        "question": "A quel astre fait référence l’autre nom de la plante herbacée appelée Monnaie du pape ?",
+        "bonne_reponse": "LA LUNE",
+        "reponse": ["LA LUNE", "MARS", "LE SOLEIL", "MON FESSIER"],
+    },
+    {
+        "question": "Quelle céréale cultivée en Ethiopie produit des graines parmi les plus petites au monde ?",
+        "bonne_reponse": "TEFF",
+        "reponse": ["TEFF", "AVOINE", "SORGHO", "MILLET"],
+    },
+    {
+        "question": "Dans quel pays d’Afrique australe se trouve le désert côtier du Namib ?",
+        "bonne_reponse": "NAMIBIE",
+        "reponse": ["NAMIBIE", "MAMIBLUE", "NAMIBOU", "NAMIGLOU"],
+    },
+    {
+        "question": "Quelle matière très résistante à la chaleur appelait-on autrefois Asbeste ?",
+        "bonne_reponse": "AMIANTE",
+        "reponse": ["AMIANTE", "CYPRINE", "CARBONE", "ZINC"],
+    },
+    {
+        "question": "De quel pays l’Ibex 35 est-il le principal indice boursier ?",
+        "bonne_reponse": "L'ESPAGNE",
+        "reponse": ["L'ESPAGNE", "LA FRANCE", "L'ALLEMAGNE", "L'ITALIE"],
+    },
+    {
+        "question": "Dans quelle grande ville américaine le Lieutenant Colombo mène-t-il ses enquêtes ?",
+        "bonne_reponse": "LOS ANGELES",
+        "reponse": ["LAS VEGAS", "MIAMI", "LOS ANGELES", "MANHATTAN"],
+    },
+    {
+        "question": "De quelle arme un objet ensiforme a-t-il l’aspect ?",
+        "bonne_reponse": "D’UNE EPÉE",
+        "reponse": ["D’UNE EPÉE", "D'UN FUSIL", "D'UN ARC", "D'UNE MATRAQUE"],
+    },
+    {
+        "question": "En halieutique, quel verbe désigne l’action de troubler l’eau pour faciliter la pêche ?",
+        "bonne_reponse": "RABOUILLER",
+        "reponse": ["BATTRE", "RABOUILLER", "BOUILLER", "URINER"],
+    },
+    {
+        "question": "Quelle expédition ayant duré 8 mois a été organisée par André Citroën sur le continent Africain ?",
+        "bonne_reponse": "LA CROISIÈRE NOIRE",
+        "reponse": ["LA CROISIÈRE NOIRE", "LA CROISIÈRE JAUNE", "LA CROISIÈRE MAGENTA", "LA CROISIÈRE C4 PICASSO"],
+    },
+    {
+        "question": "Quel mollusque bivalve est appelé clovisse en Provence ?",
+        "bonne_reponse": "LA PALOURDE",
+        "reponse": ["LA PALOURDE", "L'HUÎTRE", "LA MOULE", "LE PÉTONCLE"],
+    },
+    {
+        "question": "Dans la mythologie grecque, qu'elle néréide fut changée en fontaine par Artémis afin d’échapper à Alphée ?",
+        "bonne_reponse": "ARETHUSE",
+        "reponse": ["ARETHUSE", "AMPHITRITE", "THÉTIS", "PSAMATHÉE"],
+    },
+    {
+        "question": "Quel président Américain est mort le même jour que l’écrivain Aldous Huxley, le 22 novembre 1963 ?",
+        "bonne_reponse": "JOHN FITZGERALD KENNEDY",
+        "reponse": ["JOHN FITZGERALD KENNEDY", "HERBERT CLARK HOOVER", "JOHN TYLER", "BARACK HUSSEIN OBAMA"],
+    },
+    {
+        "question": "Sur un tableau de 1834, quel peintre représente ses 3 sœurs écrivaines Emily, Charlotte et Anne ?",
+        "bonne_reponse": "PATRICK BRANWELL BRONTË",
+        "reponse": ["PATRICK BRANWELL BRONTË", "JEAN HIPPOLYTE FLANDRIN", "GIUSEPPE ELENA", "JOSEPH MALLORD WILLIAM TURNER"],
+    },
+    {
+        "question": "Quelle est la nationalité du coureur cycliste Jarlinson Pantano ?",
+        "bonne_reponse": "COLOMBIENNE",
+        "reponse": ["ITALIENNE", "MONGOLIENNE", "CONGOLAISE", "COLOMBIENNE"],
+    },
+    {
+        "question": "En anatomie, de quelle partie du pied parle-t-on lorsque l’on évoque l’hallux ?",
+        "bonne_reponse": "LE GROS ORTEIL",
+        "reponse": ["LE GROS ORTEIL", "LE TALON", "MÉTATARSE", "LE POIGNET"],
+    },
+    {
+        "question": "Quel mot peut désigner à la fois une abstinence alimentaire, et le parlement Polonais ?",
+        "bonne_reponse": "LA DIETE",
+        "reponse": ["LA DIETE", "LA DALLE", "LA PAUVRETÉ", "LE SEVRAGE"],
+    },
+    {
+        "question": "Quel prénom est commun à la marionnette de Jeff Panacloc et à l’animateur Morandini ?",
+        "bonne_reponse": "JEAN-MARC",
+        "reponse": ["LEAN-LUC", "J'EMMERDE", "JEAN-MARC", "JEAN-CHRISTOPHE"],
+    },
+    {
+        "question": "Parmi les 7 péchés capitaux, lequel est synonyme de pingrerie ? ",
+        "bonne_reponse": "L'AVARICE",
+        "reponse": ["LA GOURMANDISE", "L'AVARICE", "LA PARESSE", "L'ORGUEIL"],
+    },
+    {
+        "question": "Quelle confiserie à base de miel, d’amandes et de blancs d’œufs est une variante espagnole du nougat ?",
+        "bonne_reponse": "LE TOURON",
+        "reponse": ["LA NOUGATINE", "LE TOURON", "LE GAZ", "PASTELLAKI"],
+    },
+    {
+        "question": "Sur quel continent peut-on croiser un serpent au corps très fin appelé serpent-liane ?",
+        "bonne_reponse": "AMÉRIQUE DU SUD",
+        "reponse": ["AMÉRIQUE DU SUD", "AMÉRIQUE DU NORD", "EUROPE", "AFRIQUE"],
+    },
 ];
-
 
 function question_aleatoire() {
     $('.reponse').css('background', '#2A0E5B').hover(function () {
@@ -51,16 +160,19 @@ function question_aleatoire() {
     });
     $('span').css('color', '#00bf00');
     $('.choix_reponse').css('color', 'white');
+
     const chiffre_ale = Math.floor(Math.random() * questObj.length);
     $.each(questObj.slice(chiffre_ale, chiffre_ale + 1), function(index , value) {
         $('#question').text(value.question);
         const reponse = value.reponse;
+
         for (let pos = reponse.length-1; pos >= 1; pos--) {
             const hasard = Math.floor(Math.random() * (pos + 1));
             const sauve = reponse[pos];
             reponse[pos] = reponse[hasard];
             reponse[hasard] = sauve;
         }
+
         $('.choix_reponse').each(function (index) {
             $(this).text(reponse[index])
         });
@@ -68,27 +180,36 @@ function question_aleatoire() {
             $('.reponse').unbind('click').hover(function () {
                 $(this).css('background', '#2A0E5B');
                 $(this).find('span').css('color', '#00bf00');
-                $(this).find('.choix_reponse').css('color', 'white')
+                $(this).find('.choix_reponse').css('color', 'white');
             });
             if ($(this).find('.choix_reponse').text() === value.bonne_reponse) {
                 $(this).css('background', 'green');
+
                 $(this).hover(function () {
                     $(this).css('background', 'green');
                 });
+
                 $(this).find('span').css('color', 'black');
+
                 $(this).hover(function () {
                     $(this).find('span').css('color', 'black')
                 });
+
                 setTimeout(question_aleatoire, 2000);
+                questObj.splice(chiffre_ale, 1);
+
             } else {
                 $(this).css('background', 'red').hover(function () {
                     $(this).css('background', 'red')
                 });
+
                 $(this).find('span').css('color', 'black');
                 $(this).hover(function () {
                     $(this).find('span').css('color', 'black')
                 });
+
                 setTimeout(question_aleatoire, 2000);
+                questObj.splice(chiffre_ale, 1);
             }
         });
     });
